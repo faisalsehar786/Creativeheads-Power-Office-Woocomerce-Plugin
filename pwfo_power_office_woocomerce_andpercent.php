1c14dbfec756b7e1e60ad73713cc541cfcba9825
faisalsehar <48986108+faisalsehar786@users.noreply.github.com>
2022-02-22 15:28:59 +0500

    revercomiit

--- pwfo_power_office_woocomerce_andpercent.php
+++ pwfo_power_office_woocomerce_andpercent.php
@@ -378,52 +378,112 @@ echo  json_encode(['status'=>400]);
 
 }
 
+if ( is_admin() ) {
+    if( ! function_exists( 'get_plugin_data' ) ) {
+        require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    }
+    $plugin_data = get_plugin_data( __FILE__ );
+
+   
 
 
+if( ! class_exists( 'mishaUpdateChecker' ) ) {
+
+	class mishaUpdateChecker{
+
+		public $plugin_slug;
+		public $version;
+		public $cache_key;
+		public $cache_allowed;
+
+		public function __construct() {
+
+			$this->plugin_slug = plugin_basename( __DIR__ );
+			$this->version = $plugin_data['Version'];
+			$this->cache_key = 'chfs_upadte_plugin';  
+			$this->cache_allowed = false;
 
 
-add_filter( 'plugins_api', 'misha_plugin_info', 20, 3);
-/*
- * $res empty at this step
- * $action 'plugin_information'
- * $args stdClass Object ( [slug] => woocommerce [is_ssl] => [fields] => Array ( [banners] => 1 [reviews] => 1 [downloaded] => [active_installs] => 1 ) [per_page] => 24 [locale] => en_US )
- */
-function misha_plugin_info( $res, $action, $args ){
+             $remoteaccess = $this->request();
+            if ($plugin_data['Version']<$remoteaccess->version) {
+            	// code...
+           
+			add_filter( 'plugins_api', array( $this, 'info' ), 20, 3 );
+			add_filter( 'site_transient_update_plugins', array( $this, 'update' ) );
+			add_action( 'upgrader_process_complete', array( $this, 'purge' ), 10, 2 );
+ }
+		}
 
-	// do nothing if this is not about getting plugin information
-	if( 'plugin_information' !== $action ) {
-		return false;
-	}
-
-	// do nothing if it is not our plugin
-	if( plugin_basename( __DIR__ ) !== $args->slug ) {
-		return false;
-	}
-
-	// info.json is the file with the actual plugin information on your server
 	
-	$remote = wp_remote_get(
-		CHFS_VALIDATE_API_URL.'/api/updatePlugindata?id='.CHFS_VALIDATE_API_PLUGIN_ID, 
-		array(
-			'timeout' => 10,
-			'headers' => array(
-				'Accept' => 'application/json'
-			) 
-		)
-	);
+		
 
-	// do nothing if we don't get the correct response from the server
-	if( 
-		is_wp_error( $remote )
-		|| 200 !== wp_remote_retrieve_response_code( $remote )
-		|| empty( wp_remote_retrieve_body( $remote ) 
-	) {
-		return false;	
-	}
+		public function request(){
 
-	$remote = json_decode( wp_remote_retrieve_body( $remote ) );
-	$res = new stdClass();
-	$res->name = $remote->name;
+
+
+
+			$remote = get_transient( $this->cache_key );
+
+			if( false === $remote || ! $this->cache_allowed ) {
+
+				$remote = wp_remote_get(
+					CHFS_VALIDATE_API_URL.'/api/updatePlugindata?id='.CHFS_VALIDATE_API_PLUGIN_ID,
+					array(
+						'timeout' => 10,
+						'headers' => array(
+							'Accept' => 'application/json'
+						)
+					)
+				);
+
+				if(
+					is_wp_error( $remote )
+					|| 200 !== wp_remote_retrieve_response_code( $remote )
+					|| empty( wp_remote_retrieve_body( $remote ) )
+				) {
+					return false;
+				}
+
+				set_transient( $this->cache_key, $remote, DAY_IN_SECONDS );
+
+			}
+
+			$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+
+
+               
+			
+			return $remote;
+
+
+
+		}
+
+
+		function info( $res, $action, $args ) {
+
+			
+
+			// do nothing if you're not getting plugin information right now
+			if( 'plugin_information' !== $action ) {
+				return false;
+			}
+
+			// do nothing if it is not our plugin
+			if( $this->plugin_slug !== $args->slug ) {
+				return false;
+			}
+
+			// get updates
+			$remote = $this->request();
+
+			if( ! $remote ) {
+				return false;
+			}
+
+			$res = new stdClass();
+
+			$res->name = $remote->name;
 			$res->slug =strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-',$remote->name))); 
 			$res->version = $remote->version;
 			$res->tested = $remote->version;
@@ -443,62 +503,59 @@ function misha_plugin_info( $res, $action, $args ){
 				);
 			
 
-		
-	
-	return $res;
+			return $res;
+
+		}
+
+		public function update( $transient ) {
+
+			if ( empty($transient->checked ) ) {
+				return $transient;
+			}
+
+			$remote = $this->request();
+    
+			if(
+				$remote
+				&& version_compare( $this->version, $remote->version, '<' )
+				// && version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )
+				// && version_compare( $remote->requires_php, PHP_VERSION, '<' )
+			) {
+				$res = new stdClass();
+				$res->slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-',$remote->name)));
+				// $res->plugin = plugin_basename( __FILE__ ); // misha-update-plugin/misha-update-plugin.php
+				$res->new_version = $remote->version;
+				$res->tested = 5.8;
+				$res->package = CHFS_VALIDATE_API_URL.'/public/'.$remote->file;
+
+			
+
+				$transient->response[ $res->plugin ] = $res;
+
+	    }
+
+			return $transient;
+
+		}
+
+		public function purge(){   
+
+			if (
+				$this->cache_allowed
+				&& 'update' === $options['action']
+				&& 'plugin' === $options[ 'type' ]
+			) {
+				// just clean the cache when new plugin version is installed
+				delete_transient( $this->cache_key );
+			}
+
+		}
+
+
+	}
+
+	new mishaUpdateChecker();
+
+ }
 
 }
-
-
-
-add_filter( 'site_transient_update_plugins', 'misha_push_update' );
- 
-function misha_push_update( $transient ){
- 
-	if ( empty( $transient->checked ) ) {
-		return $transient;
-	}
-
-	$remote = wp_remote_get( 
-		CHFS_VALIDATE_API_URL.'/api/updatePlugindata?id='.CHFS_VALIDATE_API_PLUGIN_ID, 
-		array(
-			'timeout' => 10,
-			'headers' => array(
-				'Accept' => 'application/json'
-			)
-		)
-	);
-
-	if( 
-		is_wp_error( $remote )
-		|| 200 !== wp_remote_retrieve_response_code( $remote )
-		|| empty( wp_remote_retrieve_body( $remote ) 
-	) {
-		return $transient;	
-	}
-	
-	$remote = json_decode( wp_remote_retrieve_body( $remote ) );
- 
-		// your installed plugin version should be on the line below! You can obtain it dynamically of course 
-	if(
-		$remote
-		&& version_compare( $this->version, $remote->version, '<' )
-		&& version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )
-		&& version_compare( $remote->requires_php, PHP_VERSION, '<' )
-	) {
-		
-		$res = new stdClass();
-		$res->slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-',$remote->name))); 
-		$res->plugin = plugin_basename( __FILE__ ); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
-		$res->new_version = $remote->version;
-		$res->tested = $remote->version;
-		$res->package = CHFS_VALIDATE_API_URL.'/public/'.$remote->file;
-		$transient->response[ $res->plugin ] = $res;
-		
-		//$transient->checked[$res->plugin] = $remote->version;
-	}
- 
-	return $transient;
-
-}
-
